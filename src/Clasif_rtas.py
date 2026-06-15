701879bbb855620ac4c41430db65bbc1de08d35c
TomasLopezDurand <tlopezdurand@udesa.edu.ar>
2026-06-15 18:36:28 -0300

    Contador 1-7

--- src/Clasif_rtas.py
+++ src/Clasif_rtas.py
@@ -7,8 +7,24 @@ def rtas_A_B(list_rtas, i, cont, diccio_indice):
     return cont
 
 def sumar_autoestimacion(list_rtas, i, cont, diccio_indice):
-    
-    pass
+    for lista in diccio_indice:
+        for e in diccio_indice[lista]:
+            if e == i:
+                if list_rtas[i] == "1":
+                    cont[lista] += 1
+                elif list_rtas[i] == "2":
+                    cont[lista] += 2
+                elif list_rtas[i] == "3":
+                    cont[lista] += 3
+                elif list_rtas[i] == "4":
+                    cont[lista] += 4
+                elif list_rtas[i] == "5":
+                    cont[lista] += 5
+                elif list_rtas[i] == "6":
+                    cont[lista] += 6
+                else:
+                    cont[lista] += 7
+    return cont
 
 def clasif_rtas(list_rtas, diccio_indice):
     cont = {"R": 0, "A": 0, "I": 0, "S": 0, "E": 0, "C": 0}
