03d6fcedc1433b234fef0b7fd4919f5bd6771245
TomasLopezDurand <tlopezdurand@udesa.edu.ar>
2026-06-15 18:08:56 -0300

    Update Clasif_rtas.py

--- src/Clasif_rtas.py
+++ src/Clasif_rtas.py
@@ -1,21 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 12 15:28:49 2026
-
-@author: Tomas Lopez Durand
-"""
-
-def rtas_A_B():
+def rtas_A_B(list_rtas, i, cont, diccio_indice):
+    for e in diccio_indice.values:
+        if e == i:
+            
     pass
 
-def sumar_autoestimacion():
+def sumar_autoestimacion(list_rtas, i, cont, diccio_indice):
     pass
 
 def clasif_rtas(list_rtas, diccio_indice):
     cont = {"R": 0, "A": 0, "I": 0, "S": 0, "E": 0, "C": 0}
     for i in range(len(list_rtas)):
         if i == 0:#posición
-            cont = rtas_A_B(cont, diccio_indice)
+            cont = rtas_A_B(list_rtas, i, cont, diccio_indice)
         else:
-            cont = sumar_autoestimacion(cont, diccio_indice)
+            cont = sumar_autoestimacion(list_rtas, i, cont, diccio_indice)
     return cont
