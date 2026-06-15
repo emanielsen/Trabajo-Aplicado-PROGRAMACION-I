547b4f580075e39fe35373c83a5d8cc31bbe8e28
TomasLopezDurand <tlopezdurand@udesa.edu.ar>
2026-06-15 19:19:10 -0300

    Docstrings

--- src/Clasif_rtas.py
+++ src/Clasif_rtas.py
@@ -1,32 +1,96 @@
 def rtas_A_B(list_rtas, i, cont, diccio_indice):
+    """
+    Se busca el índice de donde pertenece la pregunta y se acumula un valor específico al puntaje de la personalidad en específico.
+
+    Parameters
+    ----------
+    list_rtas : lista
+        Contiene todas las respuestas del usuario en estilo de string.
+    i : int
+        Valor índice del que se utiliza para buscar de qué personalidad pertenece la respuesta.
+    cont : diccionario
+        Es el contador de puntajes para cada personalidad del R.A.I.S.E.C. del usuario.
+    diccio_indice : diccionario
+        Diccionario con las claves de R.A.I.S.E.C. con una lista dentro de cada clave que contiene los números para saber a qué personalidad pertenece cada respuesta.
+
+    Returns
+    -------
+    cont
+        Diccionario contador.
+    """
+    
     for lista in diccio_indice:
         for e in diccio_indice[lista]:
             if e == i:
                 if list_rtas[i] == "a":
                     cont[lista] += 1
+                    break
     return cont
 
 def sumar_autoestimacion(list_rtas, i, cont, diccio_indice):
+    '''
+    Se busca el índice de donde pertenece la pregunta y se acumula un valor específico al puntaje de la personalidad en específico.
+
+    Parameters
+    ----------
+    list_rtas : lista
+        Contiene todas las respuestas del usuario en estilo de string.
+    i : int
+        Valor índice del que se utiliza para buscar de qué personalidad pertenece la respuesta.
+    cont : diccionario
+        Es el contador de puntajes para cada personalidad del R.A.I.S.E.C. del usuario.
+    diccio_indice : diccionario
+        Diccionario con las claves de R.A.I.S.E.C. con una lista dentro de cada clave que contiene los números para saber a qué personalidad pertenece cada respuesta.
+
+    Returns
+    -------
+    cont
+        Diccionario contador.
+    '''
     for lista in diccio_indice:
         for e in diccio_indice[lista]:
             if e == i:
                 if list_rtas[i] == "1":
                     cont[lista] += 1
+                    break
                 elif list_rtas[i] == "2":
                     cont[lista] += 2
+                    break
                 elif list_rtas[i] == "3":
                     cont[lista] += 3
+                    break
                 elif list_rtas[i] == "4":
                     cont[lista] += 4
+                    break
                 elif list_rtas[i] == "5":
                     cont[lista] += 5
+                    break
                 elif list_rtas[i] == "6":
                     cont[lista] += 6
+                    break
                 else:
                     cont[lista] += 7
+                    break
     return cont
 
 def clasif_rtas(list_rtas, diccio_indice):
+    '''
+    Dentro de esta función, se va a crear el diccionario cont que contiene las iniciales de las personalidades R.A.I.S.E.C.
+    Se redirige a la función rtas_A_B o sumar_autoestimacion dependiendo de qué clase de pregunta es ya sabiendo las posiciones de los tipos de respuesta.
+    Finalmente, se sobreescribe lo que acumula el diccionario cont para así poder reutilizarlo en la siguiente iteración del for.
+
+    Parameters
+    ----------
+    list_rtas : lista
+        Contiene todas las respuestas del usuario en estilo de string.
+    diccio_indice : diccionario
+        Diccionario con las claves de R.A.I.S.E.C. con una lista dentro de cada clave que contiene los números para saber a qué personalidad pertenece cada respuesta.
+
+    Returns
+    -------
+    cont
+        Diccionario contador ya filtrado con todos los puntos de las respuestas.
+    '''
     cont = {"R": 0, "A": 0, "I": 0, "S": 0, "E": 0, "C": 0}
     for i in range(len(list_rtas)):
         if i == 0:#posición
